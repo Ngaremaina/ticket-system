7c9d869042bced9f0b273859fd860a4cadf27ed0
Ngaremaina <ngaremaina4@gmail.com>
2024-02-27 06:44:10 +0300

    Updates

--- backend/models.py
+++ backend/models.py
@@ -10,3 +10,13 @@ class Auth(db.Model):
     password = db.Column(db.String(255), nullable = False)
     role = db.Column(db.String(255), nullable = False)
 
+class Profile(db.Model):
+    __tablename__ = "profile"
+
+    id = db.Column(db.Integer, primary_key = True, autoincrement = True)
+    firstname = db.Column(db.String(255), nullable = False)
+    lastname = db.Column(db.String(255), nullable = False)
+
+    auth_id = db.Column(db.Integer, db.ForeignKey('auth.id'), nullable = False)
+
+    auth = db.relationship("Auth", backref = "profile")
--- frontend/src/components/client/DashBoard.js
+++ frontend/src/components/client/DashBoard.js
@@ -1,26 +1,43 @@
 import {useEffect, useState} from "react"
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 import Signup from "./Signup"
 
 
 export default function DashBoard(){
+    const navigate = useNavigate()
 
     const [client, setClient] = useState(null)
 
     useEffect(() => {
+        
+        const fetchUser = async () => {
+            const response = await fetch("http://127.0.0.1:5000/current_user")
+            const data = await response.json()
+            return setClient(data)
+        }
         fetchUser()
     },[])
 
-    const fetchUser = async () => {
-        const response = await fetch("http://127.0.0.1:5000/current_user")
-        const data = await response.json()
-        return setClient(data)
+    
+
+    function handleLogout(){
+        fetch("http://127.0.0.1:5000/logout",{
+            method:"POST",
+            headers:{"Content-Type":"application/json"},
+
+        })
+        .then(response => response.json())
+        .then(data => console.log(data))
     }
     return(
-        <div>{client !== null ? (
-            <div>Logged in</div>
+        <div>{client ? (
+            <div>Logged in
+                <p>ID: {client.id}</p>
+                <button onClick={handleLogout}>Logout</button>
+            </div>
+            
         ) : (
-            <div><Signup/></div> 
+            navigate("/sign in")
         ) }</div>
     )
 }
