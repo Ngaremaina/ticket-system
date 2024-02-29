90de8ac2e2dfa738665421b78ee3c2926fa3231c
Ngaremaina <ngaremaina4@gmail.com>
2024-02-29 16:20:47 +0300

    Changes in the settings

--- backend/main.py
+++ backend/main.py
@@ -4,7 +4,7 @@ from werkzeug.serving import run_simple
 
 if __name__ == "__main__":
     run_simple(
-        hostname='localhost',
+        hostname='0.0.0.0',
         port=5000,
         application=app
     )
